38fce53f4b2dc20bf48e97b0faeedfe6f51e5567
Maquisarde <61286359+Maquisarde@users.noreply.github.com>
2022-08-18 02:14:24 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import createChart1 from "./chart1.js";
 import createChart2 from "./chart2.js";
 
 //
-fetch("https://www.dropbox.com/s/1mok8uvlgl4xj9i/online_rend_all.csv?dl=1")
+fetch("https://www.dropbox.com/s/1mok8uvlgl4xj9i/online_rend_all.csv?dl=1",{mode: 'no-cors'})
   .then((res) => res.text())
   .then((csvText) => {
     const series = [
@@ -19,7 +19,7 @@ fetch("https://www.dropbox.com/s/1mok8uvlgl4xj9i/online_rend_all.csv?dl=1")
   });
 
 //
-fetch("https://www.dropbox.com/s/2wewp54g6hx971l/online_rend_all_medium.csv?dl=1")
+fetch("https://www.dropbox.com/s/2wewp54g6hx971l/online_rend_all_medium.csv?dl=1",{mode: 'no-cors'})
   .then((res) => res.text())
   .then((csvText) => {
     const series = [
